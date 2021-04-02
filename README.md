34ea3c053691f14692e1c3bbd6f56f4e6cc5d95d
Angel Woo <awoo424@gmail.com>
2021-04-03 01:34:21 +0800

    Update README

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# Clojure libraries for dataframe manipulation
-A comparison of different libraries for dataframe manipulation in Clojure
+# Clojure libraries for data science
+A comparison of different libraries for dataframe manipulation and big data analytics in Clojure
 
-## Libraries
+## List of libraries
 
 | Library | Description |
 | ------- | ----------- |
 | [tech.ml.dataset](https://github.com/techascent/tech.ml.dataset) | For data processing and machine learning |
 | [Geni](https://github.com/zero-one-group/geni) | Dataframe library that runs on Apache Spark |
+|[Onyx](http://www.onyxplatform.org/)| High performance distributed computation system |
+
+
+## Tutorials for each library
+
+### tech.ml.dataset
+- [Code walkthrough](https://techascent.github.io/tech.ml.dataset/walkthrough.html)
+- [Simple examples](https://techascent.github.io/tech.ml.dataset/walkthrough.html) (data export to/from SQL)
+
+### Geni
+- [Geni Cookbook](https://github.com/zero-one-group/geni/tree/develop/docs/cookbook)
+
+### Onyx
+- [Onyx cheatsheet](http://www.onyxplatform.org/docs/cheat-sheet/latest/)
+
+
+## Performance benchmarks
+
+See [ranking](https://github.com/zero-one-group/geni-performance-benchmark/)
+
+## Useful tech stack
+
+- [Apache Arrow](https://arrow.apache.org/)
+- [Apache Parquet](https://parquet.apache.org/)
--- src/clojure_dataframe/core.clj
+++ src/clojure_dataframe/core.clj
@@ -1,50 +1,8 @@
 (ns clojure-dataframe.core
-  (:require [tech.v3.dataset :as ds]
-            [tech.v3.libs.poi :refer :all])
+  (:require [clojure-dataframe.tech-ml-dataset :refer :all]
+            ;[clojure-dataframe.geni :refer :all])
   (:gen-class))
 
-; example csv data
-(def csv-data (ds/->dataset "https://github.com/techascent/tech.ml.dataset/raw/master/test/data/stocks.csv"))
-
-; example xsl data
-(def xls-data (ds/->dataset "https://github.com/techascent/tech.ml.dataset/raw/master/test/data/file_example_XLS_1000.xls"))
-(def person-data (ds/->dataset "https://github.com/techascent/tech.ml.dataset/raw/master/test/data/file_example_XLS_1000.xls"
-  {:parser-fn {"Date" [:local-date "dd/MM/yyyy"]}}))
-
-; =========================== Testing with APIs =========================== ;
-
-; get first 5 rows
-(println (ds/head xls-data))
-(println (ds/head person-data))
-
-; df -> map
-(println (take 2 (ds/mapseq-reader csv-data)))
-
-; get columns
-(println (take 5 (xls-data "Gender")))
-
-; get meta data about columns
-(println
-  (->> csv-data
-    vals  ;synonymous with ds/columns
-    (map (fn [column]
-          (meta column)))))
-
-; get brief description of dataframe (takes some time to run)
-(println (ds/brief csv-data))  
-  
-; similar to describe()
-(println (ds/descriptive-stats csv-data))
-
-; add or columns with `assoc` and `dissoc`
-(-> csv-data
-  (assoc "always-ten" 10) ;scalar values are expanded as needed
-  (assoc "random"   (repeatedly (ds/row-count csv-data) #(rand-int 100)))
-  ds/head)
-
-(-> csv-data
-  (dissoc "price")
-  ds/head)
 
 ; =========================== Main function =========================== ;
 
