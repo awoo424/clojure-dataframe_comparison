68c51a371da7e3d18b106a0bfd73e3f24bbcc521
Angel Woo <awoo424@gmail.com>
2021-04-02 12:51:05 +0800

    Test with tech.ml.dataset APIs

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Clojure libraries for dataframe manipulation
 A comparison of different libraries for dataframe manipulation in Clojure
+
+## Libraries
+
+| Library | Description |
+| ------- | ----------- |
+| [tech.ml.dataset](https://github.com/techascent/tech.ml.dataset) | For data processing and machine learning |
+| [Geni](https://github.com/zero-one-group/geni) | Dataframe library that runs on Apache Spark |
--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [techascent/tech.ml.dataset "5.13"]]
+                 [techascent/tech.ml.dataset "5.13"]
+                 [zero.one/geni "0.0.38"]]
   :main ^:skip-aot clojure-dataframe.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
